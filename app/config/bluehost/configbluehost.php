05e9d913e8a36ca2bbadde3aeee38117b1a405a8
DeltaTango <david.thomas@elliott-thomas.com.au>
2026-03-23 16:07:47 +1100

    Initial upload

---
+++ app/config/bluehost/configbluehost.php
@@ -0,0 +1,9 @@
+;<?php die('go away'); 
+[DATABASE]
+DBSERVER = MySQL
+DBHOST= localhost
+DBNAME= gtcusgmy_mrsg
+DBUSERNAME= gtcusgmy_root
+DBPASSWORD= 826rUFDud839gboU
+DEFAULTTIMEZONE = SYSTEM
+?>
---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+;<?php die('go away pest'); ?>
+[DATABASE]
+DBSERVER = MySQL
+DBNAME= sarum964_volunteers
+DBHOST= localhost
+DBUSERNAME= sarum964_webvols
+DBPASSWORD= "v)^Z%5~f,nCy"
+DEFAULTTIMEZONE = "SYSTEM"
+
